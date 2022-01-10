0899ee18f503c4d19548dbd4aa5713e047a75227
Rahul N <rahul.nagarajmv@gmail.com>
2022-01-10 17:09:50 -0600

    connect fraud and customer microservice

--- customer/src/main/java/com/rahuln/springmicroservices/customer/CustomerService.java
+++ customer/src/main/java/com/rahuln/springmicroservices/customer/CustomerService.java
@@ -2,11 +2,14 @@ package com.rahuln.springmicroservices.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
     private final CustomerRepository repository;
+    private final RestTemplate restTemplate;
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -16,6 +19,18 @@ public class CustomerService {
 
         // Todo: check if email is valid
         // Todo: check if email is not taken
-        repository.save(customer);
+        repository.saveAndFlush(customer);
+        // Todo: check if customer is fraudster
+        FraudCheckResponse response = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (response != null && response.isFraudster()) {
+            throw new IllegalStateException("Fraudster");
+        }
+
+        // Todo: send notification
     }
 }
---
+++ customer/src/main/java/com/rahuln/springmicroservices/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.rahuln.springmicroservices.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
